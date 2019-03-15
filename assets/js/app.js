d834f9f5c2a437226ec63522dc76ac2d3450735d
Chris Armata <chris.armata@gmail.com>
2019-03-14 23:40:57 -0400

    Make gifs click-to-play

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var endpoint = "https://api.giphy.com/v1/gifs/search";
 
 var key = "8jNXGAzvOAK51LSOWXQSfBKOHmPYRM7H";
-var limit = 25;
+var limit = 10;
 var rating = "PG-13";
 var offset = 0;
 var query;
@@ -12,17 +12,22 @@ function search(url) {
   $.ajax({
     url: url
   }).then(function(response) {
-    console.log(response.data);
+    //console.log(response.data);
     response.data.forEach(imgObject => {
-      var div = $("<div>").addClass("gif-container col-4");
+      var div = $("<div>").addClass("gif-container col-12 col-md-6 col-lg-4");
       var span = $("<span>")
         .text("Rating: " + imgObject.rating.toUpperCase())
         .addClass("gif-caption");
       var img = $("<img>")
         .addClass("gif")
-        .attr("src", imgObject.images.downsized.url);
+        .attr("src", imgObject.images.downsized.url)
+        .hide();
+      var thumb = $("<img>")
+        .addClass("gif-thumb")
+        .attr("src", imgObject.images.downsized_still.url);
       div.append(span);
       div.append(img);
+      div.append(thumb);
       $("#results .row").append(div);
     });
   });
@@ -45,6 +50,21 @@ $(document).ready(function() {
       "&q=" +
       query;
 
+    $("#results .row").empty();
     search(queryUrl);
+  });
+
+  $("body").on("click", "img", function() {
+    if ($(this).attr("class") === "gif-thumb") {
+      $(this).hide();
+      $(this)
+        .prev()
+        .show();
+    } else if ($(this).attr("class") === "gif") {
+      $(this).hide();
+      $(this)
+        .next()
+        .show();
+    }
   });
 });
